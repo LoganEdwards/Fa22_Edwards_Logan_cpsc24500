ce420e8fbb6673a3c5edc3b0ee3995288812196f
LEdwa <loganmedwards@lewisu.edu>
2022-09-19 12:37:30 -0500

    Updating Pagebuilder hw

--- Fa22_cpsc24500_Homework/src/PageBuilder/PageBuilder.java
+++ Fa22_cpsc24500_Homework/src/PageBuilder/PageBuilder.java
@@ -1,5 +1,17 @@
 package PageBuilder;
 
+import java.util.Scanner;
+
 public class PageBuilder {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("What is your name? ");
+		String name = input.next();
+		System.out.println("Hi there " + name + " how old are you?");
+		int age = input.nextInt();
+		System.out.println(age);
+	}
+
 
 }
