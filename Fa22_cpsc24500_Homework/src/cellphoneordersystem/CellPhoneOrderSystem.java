af03ff73e8366bdc89b045751e839f99fccadf16
LEdwa <loganmedwards@lewisu.edu>
2022-10-05 11:35:12 -0500

    more progress on phone ordering system

--- Fa22_cpsc24500_Homework/src/cellphoneordersystem/CellPhoneOrderSystem.java
+++ Fa22_cpsc24500_Homework/src/cellphoneordersystem/CellPhoneOrderSystem.java
@@ -1,11 +1,40 @@
 package cellphoneordersystem;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class CellPhoneOrderSystem {
 		
-	public static double phoneType(Scanner scan, String[] finPhone) {
-		
+	public static double phoneType(Scanner scan, ArrayList<String> finPhone) {
+		System.out.print("What type of phone would you like? \n"
+				+ "Enter A for Android and I for Iphone: ");
+		String type = scan.next();
+		if (type.equalsIgnoreCase("A")) {
+			finPhone.add("Android"); 
+			return 799.0;
+		}
+		else if (type.equalsIgnoreCase("I")) { 
+			finPhone.add("Iphone");
+			return 849.0;
+		}
 		return 0;
+	}
+	
+	public static double screenSize(Scanner scan, ArrayList<String> finPhone) {
+		System.out.print("What size of phone screen would you like? \n"
+				+ "Enter 1 for 5.6\", 2 for 6.2\" or 3 for 6.7\": ");
+		String size = scan.next();
+		if (size.equalsIgnoreCase("1")) {
+			finPhone.add("5.6\""); 
+			return 1;
+		}
+		else if (size.equalsIgnoreCase("2")) { 
+			finPhone.add("6.2\"");
+			return 1.2;
+		}
+		else {
+		finPhone.add("6.7\"");
+		return 1.4;
+		}
 	}
 	public static int phoneNum(Scanner scan) {
 		System.out.println("How many phones would you like to purchase? ");
@@ -23,10 +52,14 @@ public class CellPhoneOrderSystem {
 		return response;
 	}
 	public static void main(String[] args) {
-		String[] finalPhonePrint = {};
+		ArrayList<String> completePhonePrint = new ArrayList<String>();
 		Scanner sc = new Scanner(System.in);
 		int numPhones = phoneNum(sc);
 		for(int i = 0; i<numPhones; i++) {
+			phoneType(sc,completePhonePrint);
+			screenSize(sc, completePhonePrint);
+			
+			//enter while loop for add-ons
 			while(choice(sc).equalsIgnoreCase("F") == false) {
 				
 				
@@ -34,6 +67,8 @@ public class CellPhoneOrderSystem {
 				
 			}
 			System.out.println("Phone order completed: \n\n\n");
+			System.out.println(completePhonePrint);
+			System.out.println();
 		} 
 	}
 	
