b0bf755bfa203b2ec9a69169fdf9143d3b432eae
LEdwa <loganmedwards@lewisu.edu>
2022-10-11 11:48:59 -0500

    adding wnba outline

--- Fa22_cpsc24500_Homework/src/wnbastats/WNBAStats.java
+++ Fa22_cpsc24500_Homework/src/wnbastats/WNBAStats.java
@@ -1,5 +1,20 @@
 package wnbastats;
 
-public class WNBAStats {
+import java.io.File;
 
+public class WNBAStats {
+	
+	
+	public static void main(String[] args) {
+		try { 
+			File wnbaInfo1 = new File("wnba.txt");
+			
+		
+		} catch (Exception ex ) {
+			
+		}
+		
+		
+	}
+	
 }
