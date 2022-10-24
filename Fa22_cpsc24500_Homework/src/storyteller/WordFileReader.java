06f47e5d2aa5f9e8a94a39e51f45f7291f29e39f
LEdwa <loganmedwards@lewisu.edu>
2022-10-24 12:50:01 -0500

    more progress on file reader

--- Fa22_cpsc24500_Homework/src/storyteller/WordFileReader.java
+++ Fa22_cpsc24500_Homework/src/storyteller/WordFileReader.java
@@ -1,24 +1,31 @@
 package storyteller;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 
 public class WordFileReader {
-	//a static function called readFile that reads the words into memory.
-	private String fileName;
-	
-	public WordFileReader(String name) {
-		fileName = name;
-	}
-	
-	
-	public static String setFileName() {
-		Scanner sc = new Scanner(System.in);
-		String name;
-		System.out.print("Enter the name of the file: ");
-		name = sc.next();
-		return name;
-	}
-	
-	
+	//a static function called readFile that reads the words into memory.	
+	//fix use of arrayList later, either split between words and type, or use all together
+	public static ArrayList<String> readFile(String fileName) {
+		try {
+			Scanner fsc = new Scanner(new File(fileName));
+			String line;
+			String[] tempParts;
+			ArrayList<String> parts = new ArrayList<String>();
+			//open file and read each line
+			while (fsc.hasNextLine()) {
+				line = fsc.nextLine().trim();
+				tempParts = line.split(" ");
+			}
+			
+			
+			fsc.close();
+			return parts;
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}	
 }
